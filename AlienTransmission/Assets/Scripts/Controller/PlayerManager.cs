b0e25ef99aff1c9c02140cffdebf519f8a54ab93
mvmijares <mvmijares@users.noreply.github.com>
2018-01-27 03:48:38 -0500

    Added camera controllers for each player

--- AlienTransmission/Assets/Scripts/Controller/PlayerManager.cs
+++ AlienTransmission/Assets/Scripts/Controller/PlayerManager.cs
@@ -2,73 +2,96 @@
 using System.Collections.Generic;
 using UnityEngine;
 using InControl;
+using UnityStandardAssets.CrossPlatformInput;
 
-public class PlayerManager : MonoBehaviour {
+namespace UnityStandardAssets.Cameras {
+    public class PlayerManager : MonoBehaviour {
 
-    List<HumanPlayer> players;
-    List<Transform> playerPositions;
-    const int maxPlayers = 4;
-    int numPlayers;
-    public GameObject playerPrefab;
-	// Use this for initialization
-	void Start () {
-        InputManager.OnDeviceDetached += OnDeviceDetached; // Add a listener for when device is detached during gameplay
-        numPlayers = 0;
-        players = new List<HumanPlayer>();
+        List<HumanPlayer> players;
+        List<Transform> playerPositions;
+        const int maxPlayers = 4;
+        int playerIndex;
+        public GameObject playerPrefab;
+        public GameObject playerCameraPrefab;
 
-        //Will change this for a better setup
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        InputDevice activeInputDevice = InputManager.ActiveDevice; // active device
-        if (activeInputDevice.DPadUp) {
-            if (!FindPlayerWithDevice(activeInputDevice)) {
-                CreateNewPlayer(activeInputDevice);
-            }
-        }
+        List<Rect> cameraViewports; // List of viewport rects for individual cameras
+        // Use this for initialization
+        void Start() {
+            InputManager.OnDeviceDetached += OnDeviceDetached; // Add a listener for when device is detached during gameplay
+            players = new List<HumanPlayer>();
+            cameraViewports = new List<Rect>();
+            //Alien
+            cameraViewports.Add(new Rect(0, 0.5f, 1f, 0.5f));
+            //Human Players
+            cameraViewports.Add(new Rect(0, 0, 0.33f, 0.5f));
+            cameraViewports.Add(new Rect(0.33f, 0, 0.34f, 0.5f));
+            cameraViewports.Add(new Rect(0.67f, 0, 0.33f, 0.5f));
 
-	}
-    HumanPlayer FindPlayerWithDevice(InputDevice inputDevice) {
-        int playerCount = players.Count;
-        for (int i = 0; i < playerCount; i++) {
-            HumanPlayer player = players[i];
-            if(player.inputDevice == inputDevice) {
-                return player;
-            }
-        }
-        return null;
-    }
-    void CreateNewPlayer(InputDevice inputDevice) {
-        if (players.Count < maxPlayers) {
-            if (playerPrefab) {
-                GameObject newPlayerObject = Instantiate(playerPrefab, new Vector3(0, 0.5f, 0), playerPrefab.transform.rotation);
-                HumanPlayer playerComponent = newPlayerObject.GetComponent<HumanPlayer>();
-                playerComponent.inputDevice = inputDevice;
+            playerIndex = 0;
+            //Will change this for a better setup
 
-
-                players.Add(playerComponent);
-                Debug.Log("New Player has joined");
-                //Instantiate a new player object
-            }
-        }
-    }
-    void OnDeviceDetached(InputDevice inputDevice) {
-        var playerCount = players.Count;
-        for (int i = 0; i < playerCount; i++) {
-            HumanPlayer player = players[i];
-            if(player.inputDevice == inputDevice) {
-                //Notify the game that player has disconnected
-                //Pause Game
-                //Give time for the player to reconnect
-                //RemovePlayer(player);
-            }
-        }
-    }
-
-    void RemovePlayer(HumanPlayer player) {
-        player.inputDevice = null;
-        //Destroy player Device
        
+        }
+
+        // Update is called once per frame
+        void Update() {
+            InputDevice activeInputDevice = InputManager.ActiveDevice; // active device
+            if (activeInputDevice.Action1) {
+                if (!FindPlayerWithDevice(activeInputDevice)) {
+                    CreateNewPlayer(activeInputDevice);
+                }
+            }
+
+        }
+        HumanPlayer FindPlayerWithDevice(InputDevice inputDevice) {
+            int playerCount = players.Count;
+            for (int i = 0; i < playerCount; i++) {
+                HumanPlayer player = players[i];
+                if (player.inputDevice == inputDevice) {
+                    return player;
+                }
+            }
+            return null;
+        }
+        void CreateNewPlayer(InputDevice inputDevice) {
+            if (players.Count < maxPlayers) {
+                if (playerPrefab) {
+                    //Setting up player
+                    GameObject newPlayerObject = Instantiate(playerPrefab, new Vector3(0, 0.5f, 0), playerPrefab.transform.rotation);
+                    HumanPlayer playerComponent = newPlayerObject.GetComponent<HumanPlayer>();
+                    playerComponent.inputDevice = inputDevice;
+
+                    //Setting up player camera
+                    GameObject newPlayerCamera = Instantiate(playerCameraPrefab, new Vector3(0, 0, 0), playerCameraPrefab.transform.rotation);
+                    ThirdPersonCamera cameraComponent = newPlayerCamera.GetComponent<ThirdPersonCamera>();
+                    cameraComponent.SetTarget(newPlayerObject.transform);
+                    cameraComponent.humanPlayer = playerComponent;
+                    cameraComponent.cam.rect = cameraViewports[playerIndex];
+
+                    playerIndex++;
+                    players.Add(playerComponent);
+                    Debug.Log("New Player has joined");
+                    //Instantiate a new player object
+                }
+            }
+        }
+        void OnDeviceDetached(InputDevice inputDevice) {
+            var playerCount = players.Count;
+            for (int i = 0; i < playerCount; i++) {
+                HumanPlayer player = players[i];
+                if (player.inputDevice == inputDevice) {
+                    //Notify the game that player has disconnected
+                    //Pause Game
+                    //Give time for the player to reconnect
+                    //RemovePlayer(player);
+                }
+            }
+        }
+
+        void RemovePlayer(HumanPlayer player) {
+            player.inputDevice = null;
+            //Destroy player Device
+
+        }
     }
 }
