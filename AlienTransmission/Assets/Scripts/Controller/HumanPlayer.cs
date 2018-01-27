616b29b5df96bf7d6bcacd5590455036ef53af81
mvmijares <mvmijares@users.noreply.github.com>
2018-01-27 00:01:27 -0500

    Added basic controller functionality

---
+++ AlienTransmission/Assets/Scripts/Controller/HumanPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+public class HumanPlayer : MonoBehaviour {
+
+
+    public InputDevice inputDevice { get; set; } //individual device set up in the player manager;
+
+    float leftHorizontal; // left stick, horizontal axis
+    float leftVertical; // left stick, vertical axis
+
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (inputDevice != null) {
+            leftHorizontal = inputDevice.LeftStickX.Value;
+            leftVertical = inputDevice.LeftStickY.Value;
+            //Debug.Log(leftHorizontal + " " + leftVertical);
+        }
+
+    }
+}
