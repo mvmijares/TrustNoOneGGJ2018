07ca494c28f51f97dd8ccf8a6121653988ed1d65
Patrick Barrett <iambarrettpat@gmail.com>
2018-01-27 03:48:02 -0500

    AddedUFO

---
+++ AlienTransmission/Assets/Scripts/UFO/CaptureZone.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptureZone : MonoBehaviour {
+
+
+
+    public Transform holdingPoint;
+    public Transform CapturePoint;
+    GameObject CapturePlayer;
+    bool PlayerCaptured;
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        PlayerCaptured = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (PlayerCaptured)
+        {
+            float step = speed * Time.deltaTime;
+            CapturePlayer.transform.position = Vector3.MoveTowards(CapturePlayer.transform.position, holdingPoint.position, step);
+
+            CapturePlayer.transform.position = holdingPoint.transform.position;
+
+
+            
+        }
+
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        
+        //transform.Translate(Vector3.forward * Time.deltaTime);
+
+        if (other.gameObject.tag == "Player" && Input.GetKeyUp(KeyCode.A))
+        {
+
+            CapturePlayer = other.gameObject;
+
+            PlayerCaptured = true;
+
+            print("working");
+            
+        }
+
+        if (other.gameObject.tag == "DropOff" && PlayerCaptured)
+        {
+            CapturePlayer.transform.position = CapturePoint.transform.position;
+            PlayerCaptured = false;
+        }
+
+    }
+}
---
+++ AlienTransmission/Assets/Scripts/UFO/UFOMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOMovement : MonoBehaviour {
+
+
+    public float playerSpeed;
+    public float turnSpeed;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+        MoveForward();
+        Turn();
+
+
+	}
+
+
+
+    void MoveForward()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, playerSpeed * Time.deltaTime, 0);
+        }
+    }
+
+    void Turn()
+    {
+        if (Input.GetKey("right")) //Right arrow key to turn right
+        {
+            transform.Rotate(-Vector3.forward * turnSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("left"))//Left arrow key to turn left
+        {
+            transform.Rotate(Vector3.forward * turnSpeed * Time.deltaTime);
+        }
+    }
+
+
+
+  
+}
